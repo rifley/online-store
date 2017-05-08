5e346bec3d2e81a14d0299edb07b8c942c34e717
Riley Watts <rileywatts.yes@gmail.com>
2017-05-08 12:54:24 -0700

    add about route and marketplace route

---
+++ src/app/marketplace/marketplace.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marketplace',
+  templateUrl: './marketplace.component.html',
+  styleUrls: ['./marketplace.component.css']
+})
+export class MarketplaceComponent {
+
+
+}
